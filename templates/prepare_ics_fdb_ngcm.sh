e6e5716f1c6e83f1a3eb18e9cd8a19076001f694
agayayav <aina.gayayavila@bsc.es>
2025-03-21 12:21:16 +0100

    fixes in binds and envs

--- templates/prepare_ics_fdb_ngcm.sh
+++ templates/prepare_ics_fdb_ngcm.sh
@@ -6,9 +6,12 @@ FDB_HOME=/gpfs/projects/ehpc01/dte/fdb
 DATA_PATH=${HPCROOTDIR}/data
 PROJE=/gpfs/projects/ehpc01
 
+mkdir -p $DATA_PATH
+mkdir -p ${HPCROOTDIR}/requests
+
 # generate the requests
 python3 ${HPCROOTDIR}/runscripts/build_requests.py --general ${HPCROOTDIR}/runscripts/general_request.yaml --model ${HPCROOTDIR}/runscripts/neuralgcm.yaml --output ${HPCROOTDIR}/requests
 
 ml singularity
 
-singularity exec --env FDB_HOME=$FDB_HOME --bind $FDB_HOME,$DATA_PATH,$PWD $PROJE/containers/gsv/gsv_v2.6.0.sif bash -c "python3 retrieve.py --requests ${HPCROOTDIR}/requests --output $DATA_PATH"
+singularity exec --env FDB_HOME=$FDB_HOME --env HPCROOTDIR=$HPCROOTDIR --bind $FDB_HOME,$DATA_PATH,$HPCROOTDIR/runscripts,$PWD $PROJE/containers/gsv/gsv_v2.6.0.sif bash -c "python3 ${HPCROOTDIR}/runscripts/retrieve.py --requests ${HPCROOTDIR}/requests --output $DATA_PATH"
