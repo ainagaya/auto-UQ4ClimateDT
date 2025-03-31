4ef37b8ac29f65ec7c6c6a895968b8f6efacffb5
agayayav <aina.gayayavila@bsc.es>
2025-03-31 10:34:22 +0200

    refactor

--- runscripts/retrieve.py
+++ runscripts/retrieve.py
@@ -1,128 +1,108 @@
 import argparse
 import os
 import yaml
-from gsv.retriever import GSVRetriever
-import xarray
-
 import logging
+import xarray
+from gsv.retriever import GSVRetriever
+
 
 def parse_args():
     """Parse command-line arguments."""
     parser = argparse.ArgumentParser(description="Retrieve GSV data")
     parser.add_argument(
         "--requests", "-c", type=str, default="request.yaml",
-        help="Path to the request folder or file")
+        help="Path to the request folder or file"
+    )
     parser.add_argument(
         "--output", "-o", type=str, default="output",
         help="Path to the output folder"
     )
-    parser.add_argument('--translator', type=str, default="False", help='File to translate variables')
+    parser.add_argument(
+        "--translator", type=str, default="False",
+        help="File to translate variables"
+    )
     return parser.parse_args()
 
 
-def process_requests(requests_path, output_path, translator=False):
+def load_request(request_path):
+    """Load a YAML request file."""
+    with open(request_path, "r") as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+
+
+def retrieve_data(gsv, mars_keys):
+    """Retrieve data using GSVRetriever."""
+    return gsv.request_data(mars_keys)
+
+
+def save_data(data, output_path, filename):
+    """Save data to a NetCDF file."""
+    data.to_netcdf(os.path.join(output_path, filename))
+
+
+def interpolate_data(data, levels):
+    """Interpolate data to specified levels."""
+    logging.info("Interpolating data to specified levels")
+    if "level" not in data.dims:
+        logging.info("Adding level dimension")
+        zero_layer = xarray.full_like(data.expand_dims(dim="level"), fill_value=0)
+        data = xarray.concat([zero_layer, data.expand_dims(dim="level")], dim="level")
+    else:
+        data.interp(level=levels)
+    return data
+
+
+def rename_variables(data, translator_file=None):
+    """Rename variables based on standard names and translator file."""
+    data_copy = data.copy()
+    for var in data_copy.variables:
+        if "standard_name" in data_copy[var].attrs:
+            original_name = var
+            standard_name = data_copy[var].attrs["standard_name"]
+            renamed_name = standard_name if standard_name != "unknown" else original_name
+            data_copy = data_copy.rename({var: renamed_name})
+
+            if translator_file and renamed_name in translator_file:
+                translated_name = translator_file[renamed_name]
+                data_copy = data_copy.rename({renamed_name: translated_name})
+    return data_copy
+
+
+def process_requests(requests_path, output_path, translator_path=None):
     """Process GSV data requests."""
     gsv = GSVRetriever()
-
-    count = 0
-
-    # initialize empty xarray dataset
     merged_dataset = xarray.Dataset()
+    translator_file = None
 
-    for request in os.listdir(requests_path):
-        logging.info(f"Processing request {request}")
+    if translator_path and os.path.exists(translator_path):
+        translator_file = load_request(translator_path)
+
+    for count, request_file in enumerate(os.listdir(requests_path)):
+        logging.info(f"Processing request {request_file}")
         try:
-            request = yaml.load(open(f"{requests_path}/{request}"), Loader=yaml.FullLoader)
-            logging.debug(f"Request: {request}")
+            request = load_request(os.path.join(requests_path, request_file))
             mars_keys = request["mars-keys"]
-            logging.debug(f"Mars keys: {mars_keys}")
-            data = gsv.request_data(mars_keys)
-            logging.info(f"Data retrieved for request {request}")
-            # save the data in a netcdf file
-            data.to_netcdf(f"{output_path}/raw-{count}.nc")
+            data = retrieve_data(gsv, mars_keys)
+            save_data(data, output_path, f"raw-{count}.nc")
+
+            if "levelist_interpol" in request:
+                levels = request["levelist_interpol"]
+                data = interpolate_data(data, levels)
+
+            data = rename_variables(data, translator_file)
+            save_data(data, os.path.join(output_path, ".."), f"interpol-{count}.nc")
+            merged_dataset = xarray.merge([merged_dataset, data], compat="override")
+
         except Exception as e:
-            logging.warning(f"Failed to retrieve data for request {request}")
+            logging.warning(f"Failed to process request {request_file}: {e}")
             continue
-        logging.info("Raw data:", data)
 
-        # data.to_netcdf(f"{output_path}/raw-{count}.nc")
-
-        if "levelist_interpol" in request:
-            # if data doesn't have the dimension "level", create it, filling it with 0
-            logging.info("Interpolating data")
-            levels = request["levelist_interpol"]
-            if "level" not in data.dims:
-                logging.info("Adding level dimension")
-                zero_layer = xarray.full_like(data.expand_dims(dim="level"), fill_value = 0)
-                #data = data.expand_dims("level")
-                # Initialize with NaN (or another default value)
-                #data = data.assign_coords(level=("level", levels))
-                #data["level"] = [0]  # First level index for the 2D values
-                # Fill the first level with the original data and the rest with 1
-                #data = data.broadcast_like(data.expand_dims(level=levels))
-                # for i, level in enumerate(levels):
-                #     if i == 0:
-                #         data.loc[{"level": level}] = data.isel(level=0)
-                #     else:
-                #         data.loc[{"level": level}] = 1
-                data = xarray.concat([zero_layer, data.expand_dims(dim="level")], dim="level")
-            else:
-                logging.info("Level dimension already exists")
-                data_interp = data.interp(level=levels)
-                # plot each level and each time step
-            logging.info("Interpolated data:", data_interp)
-        else:
-            logging.info("No interpolation needed")
-            data_interp = data
-
-        # copy the array to a new variable, so we can rename it
-        data_interp_original = data_interp.copy()
-
-        for var in data_interp_original.variables:
-            logging.debug(f"Variable: {var}")
-            if "standard_name" in data_interp_original[var].attrs:
-                logging.debug(f"Standard name: {data_interp_original[var].attrs['standard_name']}")
-                original_name = var
-                # Rename variables to standard names
-                # if the standard name is not unknown
-                if data_interp_original[var].attrs["standard_name"] == "unknown":
-                    renamed_name = original_name
-                else:
-                    renamed_name = data_interp_original[var].attrs["standard_name"]
-                
-                data_interp = data_interp.rename({var: renamed_name})
-                logging.debug(f"Renamed {original_name} to {renamed_name}")
-
-                if translator:
-                    translator_file = yaml.load(open(translator), Loader=yaml.FullLoader)
-                    # if that variable is in the translator file
-                    if renamed_name in translator_file:
-                        translated_name = translator_file[renamed_name]
-                        logging.debug(f"Translating {renamed_name} to {translated_name}")
-                        data_interp = data_interp.rename({renamed_name: translated_name})
-                    else:
-                        logging.debug(f"Variable {renamed_name} not in translator file")
-                        continue
-                # plot the variable and save it in png format
-                # data_interp[data[var].attrs["standard_name"]].plot().get_figure().savefig(f"{output_path}/{data[var].attrs['standard_name']}-{count}.png")
-
-        # Uncomment the following line to save as Zarr format -> need contianer
-        # data_interp.to_zarr("interpol.zarr")
-        data_interp.to_netcdf(f"{output_path}/../interpol-{count}.nc")
-
-        # Store all the data into a single file
-        merged_dataset = xarray.merge([merged_dataset, data_interp], compat='override')
-
-        count += 1
-
-    merged_dataset.to_zarr(f"{output_path}")
-    # merged_dataset.to_netcdf(f"{output_path}/merged.nc")
+    merged_dataset.to_zarr(output_path)
 
 
 def main():
     """Main function."""
     args = parse_args()
-    # set log level to debug
     logging.basicConfig(level=logging.DEBUG)
     process_requests(args.requests, args.output, args.translator)
 
